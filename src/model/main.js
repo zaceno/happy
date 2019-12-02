2441c81ef1b47c11ae683cd86f68e4ba00c563a2
Zacharias Enochsson <zache@axis.com>
2019-12-02 13:06:19 +0100

    wip

--- src/model/main.js
+++ src/model/main.js
@@ -1,11 +1,7 @@
 import map from 'hyperapp-map'
 import * as slides from './slides'
 import * as poll from './poll'
-
-export const init = initialSlide => ({
-    slides: slides.init(initialSlide),
-    poll: poll.init,
-})
+import dispatch from '../lib/dispatch'
 
 export const getPoll = state => state.poll
 export const mapPoll = map(getPoll, (state, poll) => ({ ...state, poll }))
@@ -14,3 +10,27 @@ export const mapSlides = map(getSlides, (state, slides) => ({
     ...state,
     slides,
 }))
+
+const pages = {
+    init: Symbol(),
+    start: Symbol(),
+    vote: Symbol(),
+    pass: Symbol(),
+    result: Symbol(),
+    cleared: Symbol(),
+}
+
+export const init = initialSlide => ({
+    slides: slides.init(initialSlide),
+    poll: poll.init,
+    page: pages.init,
+})
+
+const commitPoll = mapPoll(poll.commit)
+const resetPoll = mapPoll(poll.reset)
+
+const go = (state, page) => [
+    { ...state, page },
+    state.page === pages.vote && dispatch(commitPoll),
+    page === pages.cleared && dispatch(resetPoll),
+]
