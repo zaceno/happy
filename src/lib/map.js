a8edda79e446daa99402cf7936eab4d378e42bcd
Zacharias Enochsson <zache@axis.com>
2019-12-11 15:51:47 +0100

    wip

---
+++ src/lib/map.js
@@ -0,0 +1,88 @@
+/*
+
+makeActionMap = (extract, merge) -> action Map
+
+put any Action in it, and it will return the corresponding action
+it will first dig into a stack of payload filters to apply the transform
+to the actual function. If the actual function is in memo, it will return
+the memoized transform. Otherwise it will return a new transform of the action
+and memoize the transform. And part of that means using the
+
+*/
+const isFunc = x => typeof x === 'function'
+
+const isAction = action =>
+    isFunc(action) || (Array.isArray(action) && isAction(action[0]))
+
+const deepMap = (map, action) =>
+    isFunc(action) ? map(action) : [deepMap(map, action[0]), action[1]]
+
+const getResult = (action, state, payload) =>
+    isFunc(action)
+        ? getResult(action(state, payload), state)
+        : !Array.isArray(action)
+        ? [action]
+        : isAction(action)
+        ? getResult(
+              action[0],
+              state,
+              isFunc(action[1]) ? action[1](payload) : action[1]
+          )
+        : action
+
+const makeActionMap = (extract, merge) => {
+    let rawMap = actionFn => (
+        state,
+        data,
+        subResult = getResult(actionFn, extract(state), data),
+        fullResult = getResult(merge, state, subResult[0])
+    ) => [
+        fullResult[0],
+        ...mapEffects(actualMap, subResult.slice(1)),
+        ...fullResult.slice(1),
+    ]
+
+    let memoizedMap = actionFn => {
+        let mappedAction = memoizedMap.memo.get(actionFn)
+        if (!mappedAction) {
+            mappedAction = rawMap(actionFn)
+            memoizedMap.memo.set(actionFn, mappedAction)
+        }
+        return mappedAction
+    }
+    memoizedMap.memo = new Map()
+    let actualMap = actionStack => deepMap(memoizedMap, actionStack)
+    return actualMap
+}
+
+const mapObj = (map, obj) =>
+    Object.entries(obj)
+        .map(([k, v]) => [k, isAction(v) ? map(v) : v])
+        .reduce((o, [k, v]) => ((o[k] = v), o), {})
+
+const mapEffects = (map, effects) =>
+    effects.map(([fn, opt]) => [fn, mapObj(map, opt)])
+
+const mapVNode = (map, vnode) =>
+    !vnode
+        ? vnode
+        : Array.isArray(vnode)
+        ? vnode.map(child => mapVNode(map, child))
+        : vnode.props
+        ? {
+              ...vnode,
+              props: mapObj(map, vnode.props),
+              children: mapVNode(map, vnode.children),
+          }
+        : vnode
+
+export const make = makeActionMap
+export const view = mapVNode
+export const fx = mapEffects
+
+// export default (extract, merge, map = makeActionMap(extract, merge)) => x =>
+//     isFunc(x)
+//         ? map(x)
+//         : Array.isArray(x)
+//         ? mapEffects(map, x)
+//         : mapVNode(map, x)
--- src/main.js
+++ src/main.js
@@ -1,48 +1,96 @@
 import { h as _h } from 'hyperapp'
-const h = (tag, props, children, vnode = _h(tag, props, children)) => props.map ? props.map(vnode) : vnode
+import * as map from './lib/map'
+const unmappableMap = map => action =>
+    action._x ? action : map(((action._x = false), action))
+const unMap = action => ((action._x = true), action)
+const h = (tag, props, ...content) => {
+    if (typeof tag !== 'function' || !props.map) return _h(tag, props, content)
+    const ret = map.view(
+        unmappableMap(props.map),
+        _h(tag, props, map.view(unMap, content))
+    )
+
+    console.log('RET', ret)
+    return ret
+}
+
+//given a map, can we make an unmappable, unmap pair?
+//unmap takes actions and returns actions which
+
+const nextFrame = (f => a => [f, { a }])((d, { a }) =>
+    requestAnimationFrame(_ => d(a))
+)
 
 const navigation = (() => {
-
-    const init = x => x
-
-    const current = x => x
-    const set = x => x
-
-    const button = ({page, text, extra }) => (
+    const init = page => page
+    const go = (state, to) => to
+    const button = ({ to, label, extra }) => (
         <button
             class="navButton"
-            onmousedown={[set, page]}
-            ontouchstart={[set, page]}
+            onmousedown={[go, to]}
+            ontouchstart={[go, to]}
         >
             <p class="extraText">{extra}</p>
-            <p class="mainText">{text}</p>
+            <p class="mainText">{label}</p>
         </button>
     )
 
-    const 
+    const view = ({ state }, content) => (
+        <main class="navContainer">
+            <section class="navPage">{content}</section>
+        </main>
+    )
 
-    return { button, current }
+    return { init, button, view }
 })()
 
+const counter = (() => {
+    const init = 7
+    const incr = x => x + 1
+    const decr = y => y - 1
+    const view = ({ state }) => (
+        <p>
+            <button onclick={decr}>-</button>
+            {state}
+            <button onclick={incr}>+</button>
+        </p>
+    )
+    return { init, view }
+})()
 
-const init = {
+const init = { nav: navigation.init('init'), counter: counter.init }
 
-}
-
-const view = state => (
-    <main class="navContainer">
-        <section class="navPage">
-            <div class="message">Happiness Index Calculator</div>
-            <button
-                class="navButton"
-                onmousedown={goToStart}
-                ontouchstart={goToStart}
-            >
-                <p class="extraText">Tap here to ...</p>
-                <p class="mainText">Start</p>
-            </button>
-        </section>
-    </main>
+const navMap = map.make(
+    state => state.counter,
+    (state, counter) => ({ ...state, counter })
 )
+const counterMap = map.make(
+    state => state.counter,
+    (state, counter) => ({ ...state, counter })
+)
+
+const view = state =>
+    state.nav === 'init' ? (
+        <navigation.view state={state.nav} map={navMap}>
+            <div class="message">Happiness Index Calculator</div>
+            <counter.view state={state.counter} map={counterMap} />
+            <navigation.button
+                map={navMap}
+                to="start"
+                label="Start"
+                extra="Tap here to..."
+            />
+        </navigation.view>
+    ) : state.nav === 'start' ? (
+        <navigation.view state={state.nav} map={navMap}>
+            <div class="message">Please pass the phone to the first person</div>
+            <navigation.button
+                map={navMap}
+                to="init"
+                extra="When you're ready, tap here to..."
+                label="Vote"
+            />
+        </navigation.view>
+    ) : null
 
 export { init, view }
